8eee1a6b02671b69beaac394a72079ad0bb9406d
Shusaku Uesugi <shu@chibicode.com>
2020-05-12 16:55:54 -0700

    Remove files

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
+import Head from 'next/head'
 import '../assets/css/style.css'
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />
+const App = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <title>Strapi blog</title>
+      <link
+        rel='stylesheet'
+        href='https://fonts.googleapis.com/css?family=Staatliches'
+      />
+      <link
+        rel='stylesheet'
+        href='https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/css/uikit.min.css'
+      />
+      <script src='https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.min.js' />
+      <script src='https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit-icons.min.js' />
+      <script src='https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.js' />
+    </Head>
+    <Component {...pageProps} />
+  </>
+)
 
 export default App
